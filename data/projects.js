06194c52e167b0956627898ec449e9b0627c8e2f
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-07-07 23:40:07 -0700

    Family Matters partial project added

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,6 @@
 // import projectGif from "../public/screen-recording.gif";
 import todosGif from "../public/todos.gif";
+import familyMatters from "../public/images/family-matters.png";
 
 export const projects = [
   // {
@@ -23,5 +24,16 @@ export const projects = [
     technologies: ["JavaScript", "Express", "Handlebars", "SQLite", "Node"],
     url: "",
     githubUrl: "https://github.com/BradleyRobertTaylor/todos-project",
+  },
+  {
+    id: 2,
+    screenshot: familyMatters,
+    alt: "Screenshot of home image of Family Matters and Mentors website",
+    title: "Family Matters & Mentors",
+    description:
+      "Family Matters & Mentors is a website that allows users to schedule appointments and purchase an online 12 week course. It's built with React and Next.js.",
+    technologies: ["Next.js", "React", "JavaScript"],
+    url: "https://family-matters-mentors-gh9hvgrmd-bradleyroberttaylor.vercel.app/",
+    githubUrl: "https://github.com/BradleyRobertTaylor/family-matters-mentors",
   },
 ];
