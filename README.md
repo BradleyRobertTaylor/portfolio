cbb0b872492ddde3077e6bc8740998e26094478e
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-12-16 14:13:30 -0800

    Delete old files before adding updated files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Personal Portfolio Website
-
-Minimal and sleek portfolio website built using React, Next.js, and Framer Motion for a majority of the animations.
--- components/Logo/Logo.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Logo.module.css";
-
-export default function Logo() {
-  return (
-    <a className={`${styles.logo}`} tabIndex="0" href="/">
-      <span>Bradley Taylor</span>
-    </a>
-  );
-}
