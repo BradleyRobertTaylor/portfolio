d293cc004193e6f9d4db8ab7c6e4e2ab14d4bfa7
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-12-17 15:12:53 -0800

    Change color on home hero and about hero

--- src/components/ui/GradientText.tsx
+++ src/components/ui/GradientText.tsx
@@ -1,6 +1,6 @@
 export function GradientText({ children }: { children: React.ReactNode }) {
   return (
-    <span className="text-neutral-400 dark:bg-gradient-to-r dark:from-zinc-600 dark:via-zinc-400 dark:to-zinc-600 dark:text-transparent dark:bg-clip-text">
+    <span className="text-zinc-400 dark:bg-gradient-to-r dark:from-zinc-600 dark:via-zinc-400 dark:to-zinc-600 dark:text-transparent dark:bg-clip-text">
       {children}
     </span>
   );
